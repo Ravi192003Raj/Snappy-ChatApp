c7a7fd5a70007ea897a27d8102b3e1c270dc22d2
Ravi192003Raj <ravi192003raj@gmail.com>
2024-05-24 18:05:13 +0530

    Backend Started

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+import { Schema, model, models } from "mongoose";
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export const User = models.User || model("User", schema);
